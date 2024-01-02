735e4dce0ea348cf3d7b5accfed5d5a108ecda47
Wayskid <wayskid02@gmail.com>
2024-01-02 05:17:28 +0100

    assessment

--- client/src/pages/get_started/assessments/One.jsx
+++ client/src/pages/get_started/assessments/One.jsx
@@ -1,13 +1,11 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setAssessmentProgress } from "../../../store/features/appSlice.js";
-import { useNavigate } from "react-router-dom";
 import SelectOne from "../../../components/reuseable/selectOne.jsx";
 
 export default function One() {
   const dispatch = useDispatch();
   const assessment = useSelector((state) => state.app.assessmentFile[0]);
-  const navigate = useNavigate();
 
   useEffect(() => {
     dispatch(setAssessmentProgress(4.76));
